2f6bf9048b17f04dba69771a5626c91683a553d0
ArrobbioUmberto <arrobbio.umberto@gmail.com>
2023-04-04 16:08:55 +0200

    BONUS

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/contatti', function () {
+    return view('contatti');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/services', function () {
+    return view('services');
+});
